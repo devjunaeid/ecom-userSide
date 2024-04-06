0db9eb31cd96494359349db1322ef1a684f3bb74
junaeid <junaeid.md48@gmail.com>
2024-04-06 10:41:31 +0600

    Added & Updated: Server with DB config.

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"github.com/devjunaeid/ecom-userSide/cmd/api"
+	"github.com/devjunaeid/ecom-userSide/config"
+	"github.com/devjunaeid/ecom-userSide/db"
 	log "github.com/rs/zerolog/log"
 )
 
 func main() {
-	server := api.NewApiServer(":8080", nil)
-	var err error
+	// Initializing new Db connection.
+	// Returns db and an erros. takse Database connection url.
+	db, err := db.NewPQConn(config.Envs.DbConnectionUrl) //load from .env file via config module.
+	//Checking for database connection erros.
+	if err != nil {
+		log.Error().Msg("Failed to Connect To the Database!!")
+	} else {
+		log.Info().Msg("Connected to the database.")
+	}
+
+	server := api.NewApiServer(":8080", db)
 	err = server.Run()
 
 	if err != nil {
